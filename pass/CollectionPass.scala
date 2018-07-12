e8b11affe243e5d3abe452cdbdf7fd51b2f629f2
Ilya Kirillov <darthorimar@gmail.com>
2018-07-12 12:15:49 +0300

    Fix option collection converters

--- pass/CollectionPass.scala
+++ pass/CollectionPass.scala
@@ -1,36 +1,45 @@
 package org.jetbrains.plugins.kotlinConverter.pass
 
+import org.jetbrains.plugins.kotlinConverter
 import org.jetbrains.plugins.kotlinConverter.ast._
 import org.jetbrains.plugins.kotlinConverter.types._
 
 class CollectionPass extends Pass {
   override protected def action(ast: AST): Option[AST] = ast match {
-//    case CallExpr(NullableType(_), _, _, "Some",_, Seq(v)) =>
-//      Some(pass[Expr](v))
-//
-//    case CallExpr(ty, ft@NullableType(_), Some(obj), "map", _, Seq(p)) =>
-//      Some(CallExpr(pass[Type](ty),
-//        pass[Type](ft),
-//        Some(pass[Expr](PostExpr(pass[Type](ty), pass[Expr](obj), "?"))),
-//        "let",
-//        Seq.empty,
-//        Seq(pass[Expr](p))))
-//
-//    case CallExpr(ty, _, Some(obj), "get", _, _) =>
-//      Some(PostExpr(pass[Type](ty), pass[Expr](obj), "!!"))
-//
-//    case CallExpr(ty, funcTy, None, "Seq", typeParams, args) =>
-//      Some(CallExpr(pass[Type](ty), pass[Type](funcTy), None, "listOf", typeParams, args.map(pass[Expr])))
-//
-//    case CallExpr(ty, funcTy, Some(RefExpr(_,_, "Seq")), "empty", typeParams, Seq()) =>
-//      Some(CallExpr(pass[Type](ty), pass[Type](funcTy), None, "emptyList", typeParams, Seq()))
-//
-//    //
-    //    case CallExpr(FuncType(_, t),InvExpr(ty, Some(obj), "getOrElse"), _, Seq(p)) if obj.ty.isInstanceOf[NullableType] =>
-    //      Some(BinExpr(pass[Type](t), BinOp("?:"), obj, pass[Expr](p)))
+    // Some(x) --> x
+    case CallExpr(NullableType(_), RefExpr(_, None, "Some", _, _), Seq(v)) =>
+      Some(pass[Expr](v))
+
+    // opt.map(x => x + 1) --> opt?.let {x => x + 1}
+    case CallExpr(ty, RefExpr(refTy, Some(obj), "map", typeParams, true), Seq(p))
+      if obj.ty.isInstanceOf[NullableType] =>
+      Some(CallExpr(
+        pass[Type](ty),
+        RefExpr(pass[Type](refTy),
+          Some(kotlinConverter.ast.PostExpr(obj.ty, pass[Expr](obj), "?")),
+          "let",
+          typeParams.map(pass[TypeParam]), true),
+        Seq(pass[Expr](p))))
+
+    case CallExpr(ty, RefExpr(refTy, Some(obj), "getOrElse", _, true), Seq(p)) if obj.ty.isInstanceOf[NullableType] =>
+      Some(BinExpr(pass[Type](refTy), BinOp("?:"), obj, pass[Expr](p)))
+
+    //opt.get --> opt!!
+    case RefExpr(refTy, Some(obj), "get", _, true)
+      if obj.ty.isInstanceOf[NullableType] =>
+      Some(PostExpr(pass[Type](refTy), pass[Expr](obj), "!!"))
+
+    //Seq(1,2,3) --> listOf(1,2,3)
+    case CallExpr(ty, RefExpr(refTy, Some(obj), "Seq", typeParams, true), params) =>
+      Some(CallExpr(
+        pass[Type](ty),
+        RefExpr(pass[Type](refTy), Some(pass[Expr](obj)), "listOf", typeParams.map(pass[TypeParam]), true),
+        params.map(pass[Expr])))
+
+    //    case CallExpr(ty, Some(RefExpr(_, _, "Seq")), "empty", typeParams, Seq()) =>
+    //      Some(CallExpr(pass[Type](ty), pass[Type](funcTy), None, "emptyList", typeParams, Seq()))
 
     case _ => None
   }
-
 }
 
