cd386b32bc7cfd22d2a8f2c79213bc609cc6b8c9
Ilya Kirillov <darthorimar@gmail.com>
2018-08-21 19:41:20 +0300

    Gradle structure

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+version = "0.1"
+
+plugins {
+    id("org.jetbrains.intellij") version "0.3.6"
+    id("scala")
+}
+
+
+intellij {
+    version = "IC-2018.2"
+    pluginName = "IntellijScalaToKotlin"
+    setPlugins("Kotlin", "org.intellij.scala:2018.2.10")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile("org.scala-lang:scala-library:2.12.6")
+}
