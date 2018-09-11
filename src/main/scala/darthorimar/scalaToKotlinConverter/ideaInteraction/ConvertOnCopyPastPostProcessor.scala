43238fc6ae20b1f18ddda4dee1667341c5d53539
Ilya Kirillov <darthorimar@gmail.com>
2018-09-11 19:57:58 +0300

    WIP

--- src/main/scala/darthorimar/scalaToKotlinConverter/ideaInteraction/ConvertOnCopyPastPostProcessor.scala
+++ src/main/scala/darthorimar/scalaToKotlinConverter/ideaInteraction/ConvertOnCopyPastPostProcessor.scala
@@ -1,137 +1,107 @@
 package darthorimar.scalaToKotlinConverter.ideaInteraction
-
-import java.awt.datatransfer.{ DataFlavor, Transferable }
-import java.util.Collections
-import java.{ lang, util }
-
-import com.intellij.codeInsight.editorActions.{ CopyPastePostProcessor, TextBlockTransferableData }
-import com.intellij.openapi.editor.{ Editor, RangeMarker }
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.{ Ref, TextRange }
-import com.intellij.psi.{ PsiDocumentManager, PsiElement, PsiFile }
-import darthorimar.scalaToKotlinConverter._
-import darthorimar.scalaToKotlinConverter.ast._
-import darthorimar.scalaToKotlinConverter.definition.DefinitionGenerator
-import darthorimar.scalaToKotlinConverter.step.{ ConverterStepState, KtElementGenerator }
-import org.jetbrains.kotlin.psi.{ KtElement, KtFile }
-import org.jetbrains.plugins.hocon.CommonUtil.TextRange
-import org.jetbrains.plugins.scala.extensions._
-import org.jetbrains.plugins.scala.lang.psi.ScalaPsiElement
-import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
-
-import scala.collection.JavaConverters._
-import scala.util.Try
-
-class ConvertOnCopyPastPostProcessor extends CopyPastePostProcessor[ScalaToKotlinTransferableData] {
-
-  private def getPsiInRange(file: PsiFile, range: TextRange): PsiElement =
-    file depthFirst () filter { element =>
-      range contains element.getTextRange
-    } maxBy (_.getTextRange.getLength)
-
-  override def collectTransferableData(file: PsiFile,
-                                       editor: Editor,
-                                       startOffsets: Array[Int],
-                                       endOffsets: Array[Int]): util.List[ScalaToKotlinTransferableData] =
-    Try {
-      file match {
-        case scalaFile: ScalaFile =>
-          val data =
-            inWriteAction {
-              (startOffsets zip endOffsets) flatMap {
-                case (from, to) =>
-                  getPsiInRange(scalaFile, new TextRange(from, to)) match {
-                    case scalaPsi: ScalaPsiElement =>
-                      val (ast, state) =
-                        new ScalaPsiToAstConverter(scalaFile.getProject).convert(scalaPsi, new ConverterStepState)
-                      val data = new ScalaToKotlinData(from, to, ast, state)
-                      Seq(data)
-                    case _ => Seq.empty
-                  }
-              }
-            }
-
-          Collections.singletonList(new ScalaToKotlinTransferableData(data))
-        case _ =>
-          Collections.emptyList[ScalaToKotlinTransferableData]
-      }
-    } getOrElse Collections.emptyList[ScalaToKotlinTransferableData]
-
-  override def extractTransferableData(content: Transferable): util.List[ScalaToKotlinTransferableData] = {
-    if (content.isDataFlavorSupported(ScalaToKotlinData.dataFlavor))
-      Collections.singletonList(
-        content.getTransferData(ScalaToKotlinData.dataFlavor).asInstanceOf[ScalaToKotlinTransferableData])
-    else Collections.emptyList[ScalaToKotlinTransferableData]
-  }
-
-  override def processTransferableData(project: Project,
-                                       editor: Editor,
-                                       bounds: RangeMarker,
-                                       caretOffset: Int,
-                                       indented: Ref[lang.Boolean],
-                                       values: util.List[ScalaToKotlinTransferableData]): Unit =
-    PsiDocumentManager.getInstance(project).getPsiFile(bounds.getDocument) match {
-      case ktFile: KtFile =>
-        if (new ConvertScalaToKotlinDialog(project).showAndGet()) {
-          values.asScala foreach { transData =>
-            transData.data foreach { data =>
-              val ast      = data.ast
-              val state    = data.state
-              val document = editor.getDocument
-
-              val ktElementGenerator: KtElementGenerator =
-                (code: String) => {
-                  document.replaceString(bounds.getStartOffset, bounds.getEndOffset, code)
-                  PsiDocumentManager.getInstance(project).commitDocument(document)
-                  val generatedCodeTextRange = new TextRange(bounds.getStartOffset, bounds.getStartOffset + code.length)
-                  getPsiInRange(ktFile, generatedCodeTextRange).asInstanceOf[KtElement]
-                }
-              state.elementGenerator = Some(ktElementGenerator)
-              new AstToKotlinPsiConverter(project).convert(ast, state)
-            }
+open class ConvertOnCopyPastPostProcessor() : CopyPastePostProcessor<ScalaToKotlinTransferableData> {
+  private fun getPsiInRange(file: PsiFile, range: TextRange): PsiElement =file.depthFirst(/* ERROR converting `()`*/).filter { element -> range.contains(element.getTextRange()) }.maxBy(({ it.getTextRange().getLength() }))
+  override fun collectTransferableData(file: PsiFile, editor: Editor, startOffsets: scala.Array<Int>, endOffsets: scala.Array<Int>): List<ScalaToKotlinTransferableData> =runTry { run {
+     val match = file
+    when {
+      match is ScalaFile -> {
+         val data: scala.Array<ScalaToKotlinData> = inWriteAction { (startOffsets.zip(endOffsets)).flatMap {  val match1 = it
+        open class `(from, to)_data`(private val from: Int, private val to: Int) {
+          companion object  {
+            fun apply(from: Int, to: Int): `(from, to)_data` =`(from, to)_data`(from, to)
+            fun unapply(x: `(from, to)_data`): `(from, to)_data`? =x
           }
         }
-      case _ =>
+        val `(from, to)` by lazy {
+          if (match1 is Tuple2) {
+             val (from, to) = match1
+            if (from is Int && to is Int) {
+              return@lazy `(from, to)_data`(from, to)
+            }
+          }
+          return@lazy null
+        }
+        when {
+          `(from, to)` != null -> {
+             val (from, to) = `(from, to)`
+            run {
+               val match2 = getPsiInRange(match, TextRange(from, to))
+              when {
+                match2 is ScalaPsiElement -> {
+                   val ((ast, state)) = /* ERROR converting `new ScalaPsiToAstConverter(scalaFile.getProject).convert`*/(match2, ConverterStepState())
+                   val data: ScalaToKotlinData = ScalaToKotlinData(from, to, ast, state)
+                  listOf(data)
+                }
+                else -> {
+                  emptyList()
+                }
+              }
+            }
+          }
+          else -> throw MatchError(match1)
+        } } }
+        /* ERROR converting `Collections.singletonList(new ScalaToKotlinTransferableData(data))`*/
+      }
+      else -> {
+        Collections.emptyList<ScalaToKotlinTransferableData>()
+      }
     }
-}
-
-class ScalaToKotlinTransferableData(val data: Array[ScalaToKotlinData]) extends TextBlockTransferableData {
-  override def getFlavor: DataFlavor = ScalaToKotlinData.dataFlavor
-
-  override def getOffsetCount: Int = data.length * 2
-
-  override def getOffsets(offsets: Array[Int], index: Int): Int = {
-    var i = index
-    for (d <- data) {
-      offsets(i) = d.startOffset
-      offsets(i + 1) = d.endOffset
-      i += 2
-    }
-    i
+  } }.getOrElse { Collections.emptyList<ScalaToKotlinTransferableData>() }
+  override fun extractTransferableData(content: Transferable): List<ScalaToKotlinTransferableData> {
+    return if (content.isDataFlavorSupported(ScalaToKotlinData.dataFlavor)) /* ERROR converting `Collections.singletonList(
+        content.getTransferData(ScalaToKotlinData.dataFlavor).asInstanceOf[ScalaToKotlinTransferableData])`*/ else Collections.emptyList<ScalaToKotlinTransferableData>()
   }
+  override fun processTransferableData(project: Project, editor: Editor, bounds: RangeMarker, caretOffset: Int, indented: Ref<Boolean>, values: List<ScalaToKotlinTransferableData>): Unit {
+     val match = PsiDocumentManager.getInstance(project).getPsiFile(bounds.getDocument())
+    when {
+      match is KtFile -> {
+        if (ConvertScalaToKotlinDialog(project).showAndGet()) {
+          asScalaBufferConverter(asScalaBufferConverter(asScalaBufferConverter(asScalaBufferConverter(asScalaBufferConverter(asScalaBufferConverter(asScalaBufferConverter(asScalaBufferConverter(asScalaBufferConverter(asScalaBufferConverter(values)))))))))).asScala().foreach { transData -> transData.data.foreach { data ->  val ast: AST = data.ast
+           val state: ConverterStepState = data.state
+           val document: Document = editor.getDocument()
+           val ktElementGenerator: KtElementGenerator = { code -> document.replaceString(bounds.getStartOffset(), bounds.getEndOffset(), code)
+          PsiDocumentManager.getInstance(project).commitDocument(document)
+           val generatedCodeTextRange: TextRange = TextRange(bounds.getStartOffset(), bounds.getStartOffset + code.length)
+          (getPsiInRange(match, generatedCodeTextRange) as KtElement) }
+          state.elementGenerator = ktElementGenerator
+          /* ERROR converting `new AstToKotlinPsiConverter(project).convert`*/(ast, state) } }
+        }
+      }
+      else -> {
 
-  override def setOffsets(offsets: Array[Int], index: Int): Int = {
-    var i = index
-    for (d <- data) {
-      d.startOffset = offsets(i)
-      d.endOffset = offsets(i + 1)
-      i += 2
+      }
     }
-    i
   }
-
 }
-
-case class ScalaToKotlinData(var startOffset: Int = 0, var endOffset: Int = 0, ast: AST, state: ConverterStepState)
-
-object ScalaToKotlinData {
-  lazy val dataFlavor: DataFlavor =
-    try {
-      val dataClass = ScalaToKotlinData.getClass
-      new DataFlavor(DataFlavor.javaJVMLocalObjectMimeType + ";class=" + dataClass.getName,
-                     "KotlinReferenceData",
-                     dataClass.getClassLoader)
-    } catch {
-      case _: Throwable => null
+open class ScalaToKotlinTransferableData(public val data: scala.Array<ScalaToKotlinData>) : TextBlockTransferableData() {
+  override fun getFlavor(): DataFlavor =ScalaToKotlinData.dataFlavor
+  override fun getOffsetCount(): Int =data.length * 2
+  override fun getOffsets(offsets: scala.Array<Int>, index: Int): Int {
+     var i: Int = index
+    for (ScalaToKotlinData in data) {
+      offsets.apply(i) = d.startOffset
+      offsets.apply(i + 1) = d.endOffset
+      i.+=(2)
     }
+    return i
+  }
+  override fun setOffsets(offsets: scala.Array<Int>, index: Int): Int {
+     var i: Int = index
+    for (ScalaToKotlinData in data) {
+      d.startOffset = offsets.apply(i)
+      d.endOffset = offsets.apply(i + 1)
+      i.+=(2)
+    }
+    return i
+  }
+}
+data class ScalaToKotlinData( var startOffset: Int,  var endOffset: Int,  val ast: AST,  val state: ConverterStepState) {
+  companion object  {
+     val dataFlavor: DataFlavor = try {
+       val dataClass: Class<*> = ScalaToKotlinData.getClass()
+      DataFlavor(DataFlavor.javaJVMLocalObjectMimeType + ";class=" + dataClass.getName, "KotlinReferenceData", dataClass.getClassLoader())
+    }catch (_: Throwable) {
+      null
+    }
+  }
 }
