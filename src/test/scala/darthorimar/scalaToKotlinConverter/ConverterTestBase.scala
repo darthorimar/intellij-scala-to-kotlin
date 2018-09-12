a8e7093cff528fc29f87977fa4c93b231f55485a
Ilya Kirillov <darthorimar@gmail.com>
2018-09-12 20:25:27 +0300

    Repair tests

--- src/test/scala/darthorimar/scalaToKotlinConverter/ConverterTestBase.scala
+++ src/test/scala/darthorimar/scalaToKotlinConverter/ConverterTestBase.scala
@@ -1,49 +1,43 @@
 package darthorimar.scalaToKotlinConverter
 
-import java.io.OutputStreamWriter
-
-import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFileFactory
 import com.intellij.testFramework.LightPlatformTestCase
-import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
-import darthorimar.scalaToKotlinConverter.step.{ ConverterStepState, FileElementGenerator, KtElementGenerator }
-import kotlin.text.Charsets
 import org.jetbrains.kotlin.idea.KotlinLanguage
-import org.jetbrains.kotlin.psi.{ KtElement, KtFile, KtPsiFactory }
+import org.jetbrains.kotlin.psi.{KtFile, KtPsiFactory}
 import org.jetbrains.plugins.scala.base.ScalaLightPlatformCodeInsightTestCaseAdapter
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
-import org.jetbrains.plugins.scala.extensions._
 import org.junit.Assert._
 
 abstract class ConverterTestBase extends ScalaLightPlatformCodeInsightTestCaseAdapter {
 
   private def formatString(unformatedCode: String): String = {
     val ktPsiFactory = new KtPsiFactory(LightPlatformTestCase.getProject)
-    val ktFile       = ktPsiFactory.createFile(unformatedCode)
+    val ktFile = ktPsiFactory.createFile(unformatedCode)
     Utils.reformatKtElement(ktFile)
     val formated = ktFile.getText.trim.split('\n').filterNot(_.isEmpty).mkString("\n")
     formated
   }
 
-  class TestElementGenerator(project: Project) extends KtElementGenerator {
-    override def insertCode(text: String): KtElement = {
-      PsiFileFactory
-        .getInstance(project)
-        .createFileFromText("dummy.kt", KotlinLanguage.INSTANCE, text, true, false)
-        .asInstanceOf[KtFile]
-    }
+  def createKtFile(text: String): KtFile = {
+    PsiFileFactory
+      .getInstance(project)
+      .createFileFromText("dummy.kt", KotlinLanguage.INSTANCE, text, true, false)
+      .asInstanceOf[KtFile]
   }
+
 
   def doTest(scala: String, kotlin: String): Unit = {
     configureFromFileTextAdapter("dummy.scala", scala)
     val scalaFile = getFileAdapter.asInstanceOf[ScalaFile]
 
-    val ktFile =
-      new ScalaPsiToKotlinPsiConverter(getProjectAdapter)
-        .convert(scalaFile, new ConverterStepState(Some(new TestElementGenerator(getProjectAdapter))))
-        ._1
+    val converter = new ScalaToKotlinLanguageConverter
+
+    val result = converter.convertPsiFileToText(scalaFile)
+    val ktFile = createKtFile(result.getFirst)
+    converter.runPostProcessOperations(ktFile, result.getSecond)
+
     val formatedExpected = formatString(kotlin)
-    val formatedActual   = formatString(ktFile.getText)
+    val formatedActual = formatString(ktFile.getText)
     assertEquals(formatedExpected, formatedActual)
   }
 
